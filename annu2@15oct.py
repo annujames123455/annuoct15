bb1b5070647dd9db3ee512ed532b39090a1fc4c2
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-10-15 14:44:37 +0530

    Add files via upload

---
+++ annu2@15oct.py
@@ -0,0 +1,9 @@
+"""
+author name: annu james
+python program to check the eligibility for driving license
+"""
+age=int(input("enter your age"))
+if age>=16:
+    print('eligible for driving license')
+else:
+    print('not eligible')
